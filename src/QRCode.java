06bab6f3a5f7d4a3b83d5513ff64490d2a2de5b9
Nguyen-Cong-Nghiem <kukull.ncn@gmail.com>
2019-03-11 13:44:45 +0700

    example qr code

--- src/QRCode.java
+++ src/QRCode.java
@@ -2,6 +2,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.aztec.AztecWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.pdf417.encoder.PDF417;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import javax.swing.*;
@@ -10,15 +11,21 @@ import java.awt.image.BufferedImage;
 
 public class QRCode {
     private static ImageIcon createQRCode(String key, int width, int height) {
-        AztecWriter writer = new AztecWriter();
+
+        QRCodeWriter writer = new QRCodeWriter();
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         int white = 255 << 16 | 255 << 8 | 255;
         int black = 0;
-        BitMatrix bitMatrix = writer.encode(key, BarcodeFormat.AZTEC, width, height);
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j< height; j++) {
-                image.setRGB(i, j, bitMatrix.get(i, j) ? black : white);
+        BitMatrix bitMatrix = null;
+        try {
+            bitMatrix = writer.encode(key, BarcodeFormat.QR_CODE, width, height);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j< height; j++) {
+                    image.setRGB(i, j, bitMatrix.get(i, j) ? black : white);
+                }
             }
+        } catch (WriterException e) {
+            e.printStackTrace();
         }
         return new ImageIcon(image);
     }
@@ -27,7 +34,7 @@ public class QRCode {
         JPanel panel = new JPanel();
         panel.setLayout(new CardLayout());
         JLabel jLabel = new JLabel();
-        jLabel.setIcon(createQRCode("kenhlaptrinh.net",400,200));
+        jLabel.setIcon(createQRCode("kenhlaptrinh.net",400,400));
         panel.add(jLabel);
         panel.setLayout(new CardLayout());
         JFrame frame = new JFrame();
